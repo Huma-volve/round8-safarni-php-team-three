e08fc62ff93a93115ea49d327685c2e938c4b2a9
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-01-16 09:49:32 +0200

    update in searchcontroller

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Helper\ApiResponse;
 use App\Http\Requests\SearchRequest;
 use App\Http\Resources\SearchResource;
+use App\Http\Resources\Tours\TourCardResource;
 use App\Services\SearchService;
 use Illuminate\Http\Request;
 
@@ -19,7 +20,7 @@ class SearchController extends Controller
         $tours = $this->searchService->search($request->validated());
 
         return ApiResponse::success(
-            SearchResource::collection($tours)
+            TourCardResource::collection($tours)
         );
     }
 }
